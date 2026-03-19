1fe8883ba8c840ad961573bd6960c9b000cee0e9
MELVINJOSHUA-tech <lourdmary37@gmail.com>
2026-03-19 12:55:20 +0530

    fixed api endpoint

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ if (form) {
     };
 
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch('https://portfolio-2-vinx.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
